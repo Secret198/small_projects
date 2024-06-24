211db1e4b41fd8300229be70ad17c4be3ae25d9c
Secret198 <coconutisthebest3@gmail.com>
2024-06-24 19:37:09 +0200

    Dijkstra algo visualization with test data done

--- algo.js
+++ algo.js
@@ -114,20 +114,11 @@ function ViewPath(route){
     }
 }
 
-let nigga = {i: 5, j: 2}
-function Test(){
-    c.beginPath()
-    c.fillStyle = "pink"
-    c.fillRect(canvas.width / xCellCount * (nigga.i), canvas.height / yCellCount * nigga.j, cellWidth, cellHeight)
-    c.fill()
-    nigga.i++
-    nigga.j++
-    window.requestAnimationFrame(Test)
-}
-window.requestAnimationFrame(Test)
+let isFinished = false
 
 function DrawFrames(){
-    //await new Promise(r => setTimeout(r, 1000))
+    
+    Dijktstra()
     c.clearRect(0, 0, canvas.width, canvas.height)
     Grid()
     //Draw walls
@@ -142,16 +133,7 @@ function DrawFrames(){
         }
     }
 
-    //Draw start + end
-    c.beginPath()
-    c.fillStyle = "green"
-    c.fillRect(canvas.width / xCellCount * (startPos.j-1), canvas.height / yCellCount * (startPos.i-1), cellWidth, cellHeight)
-    c.fill()
     
-    c.beginPath()
-    c.fillStyle = "blue"
-    c.fillRect(canvas.width / xCellCount * (endPoint.j-1), canvas.height / yCellCount * (endPoint.i-1), cellWidth, cellHeight)
-    c.fill()
 
     //Draw search
     for(let i = 0;i<discovered.length;i++){
@@ -166,16 +148,38 @@ function DrawFrames(){
             c.fillRect(canvas.width / xCellCount * (discovered[i].j-1), canvas.height / yCellCount * (discovered[i].i-1), cellWidth, cellHeight)
             c.fill()
         }
-    }
-    
+    } 
+
     //Draw path
-    // for(let i = 0;i<route.length;i++){
-    //     c.beginPath()
-    //     c.fillStyle = "yellow"
-    //     c.fillRect(canvas.width / xCellCount * (route[i].j-1), canvas.height / yCellCount * (route[i].i - 1), cellWidth, cellHeight)
-    //     c.fill()
-    // }
+    for(let i = 0;i<route.length;i++){
+        c.beginPath()
+        c.fillStyle = "yellow"
+        c.fillRect(canvas.width / xCellCount * (route[i].j-1), canvas.height / yCellCount * (route[i].i - 1), cellWidth, cellHeight)
+        c.fill()
+    }
+
+    //Draw start + end
+    c.beginPath()
+    c.fillStyle = "green"
+    c.fillRect(canvas.width / xCellCount * (startPos.j-1), canvas.height / yCellCount * (startPos.i-1), cellWidth, cellHeight)
+    c.fill()
+    
+    c.beginPath()
+    c.fillStyle = "blue"
+    c.fillRect(canvas.width / xCellCount * (endPoint.j-1), canvas.height / yCellCount * (endPoint.i-1), cellWidth, cellHeight)
+    c.fill()
+
+    let reqId
+
+    
+    if(!isFinished){
+        reqId = window.requestAnimationFrame(DrawFrames)
+    }else{
+        window.cancelAnimationFrame(reqId)
+    }
 }
+
+window.requestAnimationFrame(DrawFrames)
 
 //Functionality
 
@@ -196,18 +200,22 @@ function NotDiscovered(nodeI, nodeJ, discovered){
     }
     return -1
 }
- let visited = []
-    let discovered = []
-function Dijktstra(start, end){ //Szétszedni hogy lehessen animálni
-   
-    let done = false
+let visited = []
+let discovered = []
+let done = false
 
-    discovered.push({i:start.i, j:start.j, cost:0})
-    visited.push({i:start.i, j:start.j})
-    let currentNode = {i: start.i, j: start.j, cost: 0}
+discovered.push({i:startPos.i, j:startPos.j, cost:0})
+visited.push({i:startPos.i, j:startPos.j})
+let currentNode = {i: startPos.i, j: startPos.j, cost: 0}
 
+let route = []
+let routeNode = {i: endPoint.i, j: endPoint.j}
+
+route.push(routeNode)
+function Dijktstra(){
     //Find cost of every node until end is found
-    while(visited.length < (graph.length * graph[0].length) && !done){
+   
+    if(!done){
         for(let i = -1;i<=1;i++){
             for(let j = -1;j<=1;j++){
                 if(i != 0 || j != 0){
@@ -220,7 +228,7 @@ function Dijktstra(start, end){ //Szétszedni hogy lehessen animálni
                             discovered[child].cost = currentNode.cost + 1
                         }
                     }
-                    if(currentNode.i + i == end.i && currentNode.j + j == end.j){
+                    if(currentNode.i + i == endPoint.i && currentNode.j + j == endPoint.j){
                         done = true
                     }
                 }
@@ -239,41 +247,110 @@ function Dijktstra(start, end){ //Szétszedni hogy lehessen animálni
         }
         currentNode = min
         visited.push({i: currentNode.i, j:currentNode.j})
-        window.requestAnimationFrame(DrawFrames)
     }
-   
-    //Find the path with lowest cost
-    let route = []
-    let routeNode = {i: end.i, j: end.j}
-
-    route.push(routeNode)
-
-    while(routeNode.i != start.i || routeNode.j != start.j){
-        let minCost = undefined
-        let minIndex = undefined
-        for(let i = -1;i<=1;i++){
-            for(let j = -1;j<=1;j++){
-                if(i != 0 || j != 0){
-                    let discoveredIndex = NotDiscovered(routeNode.i + i, routeNode.j + j, discovered)
-                    if(discoveredIndex != -1){
-                        if(minCost == undefined || discovered[discoveredIndex].cost < minCost){
-                            minCost = discovered[discoveredIndex].cost
-                            minIndex = discoveredIndex
+    else{
+        while(routeNode.i != startPos.i || routeNode.j != startPos.j){
+            let minCost = undefined
+            let minIndex = undefined
+            for(let i = -1;i<=1;i++){
+                for(let j = -1;j<=1;j++){
+                    if(i != 0 || j != 0){
+                        let discoveredIndex = NotDiscovered(routeNode.i + i, routeNode.j + j, discovered)
+                        if(discoveredIndex != -1){
+                            if(minCost == undefined || discovered[discoveredIndex].cost < minCost){
+                                minCost = discovered[discoveredIndex].cost
+                                minIndex = discoveredIndex
+                            }
                         }
                     }
-                }
             
+                }
             }
+            route.push({i:discovered[minIndex].i, j:discovered[minIndex].j})
+            routeNode = {i:discovered[minIndex].i, j:discovered[minIndex].j}
+           
         }
-        route.push({i:discovered[minIndex].i, j:discovered[minIndex].j})
-        routeNode = {i:discovered[minIndex].i, j:discovered[minIndex].j}
+        isFinished = true
     }
-
-    
   //  ViewPath(route)
 }
+ 
+// function Dijktstra(start, end){ //Szétszedni hogy lehessen animálni
+//     let visited = []
+//     let discovered = []
+//     let done = false
 
-Dijktstra(startPos, endPoint)
+//     discovered.push({i:start.i, j:start.j, cost:0})
+//     visited.push({i:start.i, j:start.j})
+//     let currentNode = {i: start.i, j: start.j, cost: 0}
+
+//     //Find cost of every node until end is found
+//     while(visited.length < (graph.length * graph[0].length) && !done){
+//         for(let i = -1;i<=1;i++){
+//             for(let j = -1;j<=1;j++){
+//                 if(i != 0 || j != 0){
+//                     if(graph[currentNode.i + i][currentNode.j + j] && NotVisited(currentNode.i + i, currentNode.j + j, visited)&& NotDiscovered(currentNode.i + i, currentNode.j + j, discovered) == -1){
+//                         discovered.push({i: currentNode.i+i, j: currentNode.j+j, cost: currentNode.cost+1})
+//                     }else if(graph[currentNode.i + i][currentNode.j + j] && NotVisited(currentNode.i + i, currentNode.j + j, visited)){
+//                         let child = NotDiscovered(currentNode.i + i, currentNode.j + j, discovered) 
+                    
+//                         if(discovered[child].cost < currentNode.cost + 1){
+//                             discovered[child].cost = currentNode.cost + 1
+//                         }
+//                     }
+//                     if(currentNode.i + i == end.i && currentNode.j + j == end.j){
+//                         done = true
+//                     }
+//                 }
+//             }
+//         }
+//         let min = {
+//             i: undefined,
+//             j: undefined,
+//             cost: undefined
+//         }
+      
+//         for(let i = 0;i<discovered.length;i++){
+//             if((min.i == undefined || discovered[i].cost < min.cost) && NotVisited(discovered[i].i, discovered[i].j, visited)){
+//                 min = discovered[i]
+//             }
+//         }
+//         currentNode = min
+//         visited.push({i: currentNode.i, j:currentNode.j})
+//         window.requestAnimationFrame(DrawFrames)
+//     }
+   
+//     //Find the path with lowest cost
+//     let route = []
+//     let routeNode = {i: end.i, j: end.j}
+
+//     route.push(routeNode)
+
+//     while(routeNode.i != start.i || routeNode.j != start.j){
+//         let minCost = undefined
+//         let minIndex = undefined
+//         for(let i = -1;i<=1;i++){
+//             for(let j = -1;j<=1;j++){
+//                 if(i != 0 || j != 0){
+//                     let discoveredIndex = NotDiscovered(routeNode.i + i, routeNode.j + j, discovered)
+//                     if(discoveredIndex != -1){
+//                         if(minCost == undefined || discovered[discoveredIndex].cost < minCost){
+//                             minCost = discovered[discoveredIndex].cost
+//                             minIndex = discoveredIndex
+//                         }
+//                     }
+//                 }
+            
+//             }
+//         }
+//         route.push({i:discovered[minIndex].i, j:discovered[minIndex].j})
+//         routeNode = {i:discovered[minIndex].i, j:discovered[minIndex].j}
+//     }
+
+    
+//   //  ViewPath(route)
+// }
+
 
 let mousePos = {
     x: 0,
